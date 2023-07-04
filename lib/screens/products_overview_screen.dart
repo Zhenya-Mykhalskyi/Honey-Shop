950703bfa66d86ac491a12c317ce3b71815115fd
Zhenya-Mykhalskyi <zmykhalskyi@gmail.com>
2023-07-04 16:18:47 +0300

    feat: categories in ProductsOverviewScreen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -4,17 +4,162 @@ import 'package:flutter/material.dart';
 
 import '../widgets/product_item.dart';
 
-class ProductScreen extends StatelessWidget {
-  const ProductScreen({super.key});
+//Кнопка категорії
+class TabButton extends StatelessWidget {
+  final String text;
+  final bool isActive;
+  final VoidCallback onPressed;
+
+  const TabButton({
+    super.key,
+    required this.text,
+    required this.isActive,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          color: isActive
+              ? const Color.fromARGB(255, 255, 179, 0)
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              color: isActive ? Colors.transparent : Colors.white, width: 1),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Text(
+            text,
+            style: TextStyle(
+              fontFamily: 'MA',
+              fontSize: 17,
+              fontWeight: FontWeight.w600,
+              color: isActive ? Colors.black : Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Сітка в залежності від категорії
+class ProductGrid extends StatelessWidget {
+  final Stream<QuerySnapshot> stream;
+
+  const ProductGrid({super.key, required this.stream});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: stream,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Text('No products found');
+        } else {
+          // Отримуємо список документів
+          final documents = snapshot.data!.docs;
+          return Padding(
+            padding: const EdgeInsets.only(right: 20, left: 20, top: 20),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 25,
+                mainAxisSpacing: 20,
+                childAspectRatio: 1 / 1.8,
+              ),
+              itemCount: documents.length,
+              itemBuilder: (context, index) {
+                final title = documents[index].get('title');
+                final price = documents[index].get('price');
+                final imageUrl = documents[index].get('imageUrl');
+
+                return ProductItem(
+                  title: title,
+                  price: price,
+                  imageUrl: imageUrl,
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class ProductScreen extends StatefulWidget {
+  const ProductScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late int activeIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 1, vsync: this);
+    activeIndex = 0;
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final CollectionReference productsCollection =
         FirebaseFirestore.instance.collection('products');
-
     return Scaffold(
       appBar: AppBar(
-        // bottom: TabBar,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              children: [
+                TabButton(
+                  text: 'мед',
+                  isActive: activeIndex == 0,
+                  onPressed: () {
+                    setState(() {
+                      activeIndex = 0;
+                    });
+                  },
+                ),
+                const SizedBox(width: 10),
+                TabButton(
+                  text: 'інші продукти',
+                  isActive: activeIndex == 1,
+                  onPressed: () {
+                    setState(() {
+                      activeIndex = 1;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
         toolbarHeight: 100,
         elevation: 0,
         leading: Container(
@@ -56,41 +201,21 @@ class ProductScreen extends StatelessWidget {
           )
         ],
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: productsCollection.snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return const Text('No products found');
-          } else {
-            // Отримуємо список документів
-            final documents = snapshot.data!.docs;
-            return Padding(
-              padding: const EdgeInsets.only(right: 20, left: 20, top: 30),
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 25,
-                  childAspectRatio: 1 / 1.8,
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          activeIndex == 0
+              ? ProductGrid(
+                  stream: productsCollection
+                      .where('isHoney', isEqualTo: true)
+                      .snapshots(),
+                )
+              : ProductGrid(
+                  stream: productsCollection
+                      .where('isHoney', isEqualTo: false)
+                      .snapshots(),
                 ),
-                itemCount: documents.length,
-                itemBuilder: (context, index) {
-                  final title = documents[index].get('title');
-                  final price = documents[index].get('price');
-                  final imageUrl = documents[index].get('imageUrl');
-
-                  return ProductItem(
-                      title: title, price: price, imageUrl: imageUrl);
-                },
-              ),
-            );
-          }
-        },
+        ],
       ),
     );
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:honey/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String title;
@@ -12,27 +13,28 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(
-              left: 14, right: 14, top: 15, bottom: 10), //
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  AspectRatio(
-                    aspectRatio: 1, //відношення сторін 1:1
-                    child: GestureDetector(
-                      onTap: () {},
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const ProductDetailScreen())),
+      child: GridTile(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 14, right: 14, top: 15, bottom: 10), //
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 1, //відношення сторін 1:1
                       child: Hero(
                         tag: 'prodImg', //для анімації на детальній сторінці
                         child: ClipRRect(
@@ -44,80 +46,80 @@ class ProductItem extends StatelessWidget {
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 16,
+                    const SizedBox(height: 10),
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 7),
-                  Row(
-                    children: [
-                      Text(
-                        '₴${price.toString()}',
-                        style: const TextStyle(
-                            fontSize: 17, fontWeight: FontWeight.w500),
-                      ),
-                      const Text(
-                        ' / 0.5 л',
-                        style: TextStyle(
-                            fontSize: 13, fontWeight: FontWeight.w500),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-
-              //ЛІЧИЛЬНИК ЛІЧИЛЬНИК ЛІЧИЛЬНИК ЛІЧИЛЬНИК ЛІЧИЛЬНИК
-              Container(
-                height: MediaQuery.of(context).size.height * 0.04,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: const Color.fromARGB(255, 65, 65, 65),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.093,
-                        decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 255, 179, 0)
-                                .withOpacity(0.85),
-                            borderRadius: BorderRadius.circular(15)),
-                        child: const Center(
-                          child:
-                              Icon(Icons.add, size: 20.0, color: Colors.black),
+                    const SizedBox(height: 7),
+                    Row(
+                      children: [
+                        Text(
+                          '₴${price.toString()}',
+                          style: const TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.w500),
                         ),
-                      ),
-                    ),
-                    const Text(
-                      '0.5 л',
-                      style: TextStyle(
-                        fontSize: 13.0,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.093,
-                        decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 255, 179, 0)
-                                .withOpacity(0.85),
-                            borderRadius: BorderRadius.circular(15)),
-                        child: const Center(
-                          child: Icon(Icons.remove,
-                              size: 20.0, color: Colors.black),
+                        const Text(
+                          ' / 0.5 л',
+                          style: TextStyle(
+                              fontSize: 13, fontWeight: FontWeight.w500),
                         ),
-                      ),
+                      ],
                     ),
                   ],
                 ),
-              )
-            ],
+
+                //ЛІЧИЛЬНИК ЛІЧИЛЬНИК ЛІЧИЛЬНИК ЛІЧИЛЬНИК ЛІЧИЛЬНИК
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.04,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color.fromARGB(255, 65, 65, 65),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.093,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 255, 179, 0)
+                                  .withOpacity(0.85),
+                              borderRadius: BorderRadius.circular(15)),
+                          child: const Center(
+                            child: Icon(Icons.remove,
+                                size: 20.0, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      const Text(
+                        '0.5 л',
+                        style: TextStyle(
+                          fontSize: 13.0,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.093,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 255, 179, 0)
+                                  .withOpacity(0.85),
+                              borderRadius: BorderRadius.circular(15)),
+                          child: const Center(
+                            child: Icon(Icons.add,
+                                size: 20.0, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
