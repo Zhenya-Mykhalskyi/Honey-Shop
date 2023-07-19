1a2bb35e14bd9401ecbeaa9cd9f34b702e4d4b4e
Zhenya-Mykhalskyi <zmykhalskyi@gmail.com>
2023-07-19 16:03:37 +0300

    fix(style): CartItem paddings

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -62,96 +62,115 @@ class _CartItemState extends State<CartItem> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Flexible(
-                          child: Text(
-                            widget.product!.title,
-                            style: const TextStyle(
-                              fontSize: 19,
-                              fontWeight: FontWeight.w400,
-                            ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.product!.title,
+                                style: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  Text(
+                                    '₴${widget.product?.price.toStringAsFixed(2)}',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                  const Text(
+                                    ' / 0.5',
+                                    style: TextStyle(
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
-                        IconButton(
-                            icon: const Icon(Icons.delete),
-                            color: Colors.white.withOpacity(0.6),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return Dialog(
-                                    backgroundColor:
-                                        const Color.fromARGB(255, 27, 27, 27),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(30.0),
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          const SizedBox(height: 15),
-                                          const Text(
-                                            'Впевнені, що хочете видалити товар з корзини?',
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                              fontFamily: 'MA',
-                                            ),
-                                          ),
-                                          const SizedBox(height: 15),
-                                          ButtonBar(
-                                            alignment:
-                                                MainAxisAlignment.spaceAround,
-                                            children: [
-                                              TextButton(
-                                                onPressed: () async {
-                                                  Navigator.of(context).pop();
-                                                  cartProvider
-                                                      .removeItemFromCart(
-                                                          widget.productId);
-                                                  productProvider.resetLiters(
-                                                      widget.product);
-                                                },
-                                                child: const Text(
-                                                  'Так',
-                                                  style: TextStyle(
-                                                      color: Colors.red,
-                                                      fontFamily: 'MA'),
-                                                ),
-                                              ),
-                                              TextButton(
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                                child: const Text(
-                                                  'Скасувати',
-                                                  style: TextStyle(
-                                                      color: Colors.white,
-                                                      fontFamily: 'MA'),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ],
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 18),
+                          child: IconButton(
+                              icon: const Icon(Icons.delete),
+                              color: Colors.white.withOpacity(0.6),
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return Dialog(
+                                      backgroundColor:
+                                          const Color.fromARGB(255, 27, 27, 27),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15),
                                       ),
-                                    ),
-                                  );
-                                },
-                              );
-                            })
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(30.0),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            const SizedBox(height: 15),
+                                            const Text(
+                                              'Впевнені, що хочете видалити товар з корзини?',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                fontFamily: 'MA',
+                                              ),
+                                            ),
+                                            const SizedBox(height: 15),
+                                            ButtonBar(
+                                              alignment:
+                                                  MainAxisAlignment.spaceAround,
+                                              children: [
+                                                TextButton(
+                                                  onPressed: () async {
+                                                    Navigator.of(context).pop();
+                                                    cartProvider
+                                                        .removeItemFromCart(
+                                                            widget.productId);
+                                                    productProvider.resetLiters(
+                                                        widget.product);
+                                                  },
+                                                  child: const Text(
+                                                    'Так',
+                                                    style: TextStyle(
+                                                        color: Colors.red,
+                                                        fontFamily: 'MA'),
+                                                  ),
+                                                ),
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                  child: const Text(
+                                                    'Скасувати',
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontFamily: 'MA'),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
+                              }),
+                        )
                       ],
                     ),
                     const SizedBox(height: 8),
-                    Text(
-                      '₴${widget.product?.price.toStringAsFixed(2)} / 0.5',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16,
-                      ),
-                    ),
-                    const SizedBox(height: 25),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
