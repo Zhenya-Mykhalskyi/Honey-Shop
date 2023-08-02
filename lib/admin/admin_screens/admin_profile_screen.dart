bb1f906f23cf0d811f9fe02be12e437107f780fe
Zhenya-Mykhalskyi <zmykhalskyi@gmail.com>
2023-08-02 15:29:39 +0300

    feat: add CircularProgressIndicator to AdminProfileScreen

--- lib/admin/admin_screens/admin_profile_screen.dart
+++ lib/admin/admin_screens/admin_profile_screen.dart
@@ -24,6 +24,7 @@ class _AdminProfileScreenState extends State<AdminProfileScreen> {
   String? _aboutStoreText;
   Key _profileImageKey = UniqueKey();
   List<Map<String, String>> _salesPoints = [];
+  bool? _isLoading;
 
   @override
   initState() {
@@ -33,6 +34,9 @@ class _AdminProfileScreenState extends State<AdminProfileScreen> {
 
   Future<void> _fetchAdminData() async {
     try {
+      setState(() {
+        _isLoading = true;
+      });
       final user = FirebaseAuth.instance.currentUser;
       if (user != null) {
         final userDoc = await FirebaseFirestore.instance
@@ -68,6 +72,10 @@ class _AdminProfileScreenState extends State<AdminProfileScreen> {
       }
     } catch (e) {
       print("Error fetching user data: $e");
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
@@ -83,189 +91,199 @@ class _AdminProfileScreenState extends State<AdminProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const TitleAppBar(title: 'Інформація про нас'),
-      body: SingleChildScrollView(
-        primary: true,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
-          child: Column(
-            children: [
-              Card(
-                color: Colors.white.withOpacity(0.1),
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(9),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.all(12),
-                  child: IntrinsicHeight(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: AspectRatio(
-                            aspectRatio: 1,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                border: Border.all(
-                                  color: Colors.white,
-                                ),
-                              ),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: _adminImageUrl == null
-                                    ? const Icon(
-                                        Icons.person_rounded,
-                                        color: Colors.white,
-                                        size: 85,
-                                      )
-                                    : AspectRatio(
-                                        aspectRatio: 1,
-                                        child: CachedNetworkImage(
-                                          key: _profileImageKey,
-                                          placeholder: (context, url) =>
-                                              const CircularProgressIndicator(
-                                            color: AppColors.primaryColor,
-                                          ),
-                                          imageUrl: _adminImageUrl!,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 14),
-                        Expanded(
-                          flex: 2,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: _isLoading!
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: AppColors.primaryColor,
+              ),
+            )
+          : SingleChildScrollView(
+              primary: true,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+                child: Column(
+                  children: [
+                    Card(
+                      color: Colors.white.withOpacity(0.1),
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(9),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(12),
+                        child: IntrinsicHeight(
+                          child: Row(
                             children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Padding(
-                                    padding: EdgeInsets.only(bottom: 10),
-                                    child: Text(
-                                      'Контакти',
-                                      style: TextStyle(
-                                          color: AppColors.primaryColor,
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 18),
+                              Expanded(
+                                flex: 1,
+                                child: AspectRatio(
+                                  aspectRatio: 1,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      border: Border.all(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: _adminImageUrl == null
+                                          ? const Icon(
+                                              Icons.person_rounded,
+                                              color: Colors.white,
+                                              size: 85,
+                                            )
+                                          : AspectRatio(
+                                              aspectRatio: 1,
+                                              child: CachedNetworkImage(
+                                                key: _profileImageKey,
+                                                placeholder: (context, url) =>
+                                                    const CircularProgressIndicator(
+                                                  color: AppColors.primaryColor,
+                                                ),
+                                                imageUrl: _adminImageUrl!,
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
                                     ),
                                   ),
-                                  ProfileInfoCardSingleRow(
-                                    icon: Icons.person_2_outlined,
-                                    text: _adminName.toString(),
-                                  ),
-                                  const SizedBox(height: 9),
-                                  ProfileInfoCardSingleRow(
-                                    icon: Icons.email_outlined,
-                                    text: _adminEmail.toString(),
-                                  ),
-                                  const SizedBox(height: 9),
-                                  ProfileInfoCardSingleRow(
-                                    icon: Icons.phone_outlined,
-                                    text: _adminPhoneNumber.toString(),
-                                  ),
-                                ],
+                                ),
+                              ),
+                              const SizedBox(width: 14),
+                              Expanded(
+                                flex: 2,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        const Padding(
+                                          padding: EdgeInsets.only(bottom: 10),
+                                          child: Text(
+                                            'Контакти',
+                                            style: TextStyle(
+                                                color: AppColors.primaryColor,
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 18),
+                                          ),
+                                        ),
+                                        ProfileInfoCardSingleRow(
+                                          icon: Icons.person_2_outlined,
+                                          text: _adminName.toString(),
+                                        ),
+                                        const SizedBox(height: 9),
+                                        ProfileInfoCardSingleRow(
+                                          icon: Icons.email_outlined,
+                                          text: _adminEmail.toString(),
+                                        ),
+                                        const SizedBox(height: 9),
+                                        ProfileInfoCardSingleRow(
+                                          icon: Icons.phone_outlined,
+                                          text: _adminPhoneNumber.toString(),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             ],
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                child: Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Text(
-                        'Точки продажу',
-                        style: TextStyle(
-                            color: AppColors.primaryColor,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 18),
                       ),
                     ),
-                    StaggeredGridView.countBuilder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 8,
-                      itemCount: _salesPoints.length,
-                      staggeredTileBuilder: (index) =>
-                          const StaggeredTile.fit(1),
-                      itemBuilder: (context, index) {
-                        return Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(9)),
-                          color: Colors.white.withOpacity(0.1),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                const Icon(
-                                  Icons.location_on_outlined,
+                    SizedBox(
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Text(
+                              'Точки продажу',
+                              style: TextStyle(
                                   color: AppColors.primaryColor,
-                                ),
-                                const SizedBox(width: 8),
-                                Expanded(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 18),
+                            ),
+                          ),
+                          StaggeredGridView.countBuilder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 8,
+                            mainAxisSpacing: 8,
+                            itemCount: _salesPoints.length,
+                            staggeredTileBuilder: (index) =>
+                                const StaggeredTile.fit(1),
+                            itemBuilder: (context, index) {
+                              return Card(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(9)),
+                                color: Colors.white.withOpacity(0.1),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
                                     children: [
-                                      Text(
-                                        _salesPoints[index]['city'].toString(),
-                                        style: const TextStyle(
-                                            fontSize: 15,
-                                            fontWeight: FontWeight.w600),
+                                      const Icon(
+                                        Icons.location_on_outlined,
+                                        color: AppColors.primaryColor,
                                       ),
-                                      const SizedBox(
-                                        height: 2,
+                                      const SizedBox(width: 8),
+                                      Expanded(
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              _salesPoints[index]['city']
+                                                  .toString(),
+                                              style: const TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                            const SizedBox(
+                                              height: 2,
+                                            ),
+                                            Text(
+                                                _salesPoints[index]['address']
+                                                    .toString(),
+                                                style: const TextStyle(
+                                                  fontSize: 15,
+                                                )),
+                                          ],
+                                        ),
                                       ),
-                                      Text(
-                                          _salesPoints[index]['address']
-                                              .toString(),
-                                          style: const TextStyle(
-                                            fontSize: 15,
-                                          )),
                                     ],
                                   ),
                                 ),
-                              ],
+                              );
+                            },
+                          ),
+                          Text(
+                            _aboutStoreText == ''
+                                ? 'Опис магазину'
+                                : _aboutStoreText.toString(),
+                            style: const TextStyle(
+                              fontSize: 16,
                             ),
                           ),
-                        );
-                      },
-                    ),
-                    Text(
-                      _aboutStoreText == ''
-                          ? 'Опис магазину'
-                          : _aboutStoreText.toString(),
-                      style: const TextStyle(
-                        fontSize: 16,
+                          const MyDivider(),
+                          const Text('сертифікат'),
+                          const MyDivider(),
+                          ElevatedButton(
+                            onPressed: () => _logout(context),
+                            child: const Text('Вийти з акаунту'),
+                          ),
+                        ],
                       ),
-                    ),
-                    const MyDivider(),
-                    const Text('сертифікат'),
-                    const MyDivider(),
-                    ElevatedButton(
-                      onPressed: () => _logout(context),
-                      child: const Text('Вийти з акаунту'),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
