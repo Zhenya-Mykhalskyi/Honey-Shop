95ab2c135cbcc23b91d3b926872d96ef7f252358
Zhenya-Mykhalskyi <zmykhalskyi@gmail.com>
2023-08-22 12:43:26 +0300

    dark mode by default

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
@@ -30,12 +29,7 @@ class ThemeProvider extends ChangeNotifier {
   }
 
   bool get isDarkMode {
-    if (themeMode == ThemeMode.system) {
-      final brightness = SchedulerBinding.instance.window.platformBrightness;
-      return brightness == Brightness.dark;
-    } else {
-      return themeMode == ThemeMode.dark;
-    }
+    return themeMode == ThemeMode.dark;
   }
 
   void toggleTheme(bool isOn) {
